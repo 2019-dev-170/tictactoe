14189537c3b53e7c445e169ad7a22145fe07610d
2019-dev-170 <2019dev170@gmail.com>
2019-06-03 01:30:57 +0530

    Handling click post a win and refactoring

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,68 +1,69 @@
-import React, {Component} from 'react';
-import './Game.css';
+import React, { Component } from "react";
+import "./Game.css";
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: Array(9).fill(null),
+      currentPlayer: "X",
+      winner: null
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            board : Array(9).fill(null),
-            currentPlayer : "X",
-            winner : null
-        }
+  checkWinner() {
+    let winCombinations = [
+      ["0", "1", "2"],
+      ["3", "4", "5"],
+      ["6", "7", "8"],
+      ["0", "3", "6"],
+      ["1", "4", "7"],
+      ["2", "5", "8"],
+      ["0", "4", "8"],
+      ["2", "4", "6"]
+    ];
+
+    for (let i = 0; i < winCombinations.length; i++) {
+      const [a, b, c] = winCombinations[i];
+      if (
+        this.state.board[a] &&
+        this.state.board[a] === this.state.board[b] &&
+        this.state.board[a] === this.state.board[c]
+      ) {
+        this.setState({
+          winner: this.state.currentPlayer
+        });
+      }
     }
+  }
 
-    checkWinner(){
-        let winCombinations = [
-            ["0" , "1" , "2"],
-            ["3" , "4" , "5"],
-            ["6" , "7" , "8"],
-            ["0" , "3" , "6"],
-            ["1" , "4" , "7"],
-            ["2" , "5" , "8"],
-            ["0" , "4" , "8"],
-            ["2" , "4" , "6"]
-        ]
-
-        for(let i=0; i< winCombinations.length ; i++){
-            const [a,b,c] = winCombinations[i];
-            if(this.state.board[a] && (this.state.board[a] === this.state.board[b]) && (this.state.board[a] === this.state.board[c])){
-                this.setState({
-                    winner :  this.state.currentPlayer 
-                })    
-            }
-        }
+  handleSquareClick(index) {
+    if (!this.state.winner && this.state.board[index] === null) {
+      let newBoard = this.state.board;
+      newBoard[index] = this.state.currentPlayer;
+      this.setState({
+        currentPlayer: this.state.currentPlayer === "X" ? "O" : "X",
+        board: newBoard
+      });
+      this.checkWinner();
     }
+  }
 
-    handleSquareClick(index){
+  renderGrid() {
+    return this.state.board.map((box, index) => (
+      <div
+        className="box"
+        key={index}
+        onClick={() => this.handleSquareClick(index)}
+      >
+        {box}
+      </div>
+    ));
+  }
 
-        if(this.state.board[index] === null){
-            let newBoard = this.state.board;
-            newBoard[index] = this.state.currentPlayer; 
-            this.setState({
-                currentPlayer:  this.state.currentPlayer === "X" ? "O" : "X",
-                board :  newBoard        
-            })
-            this.checkWinner();
-        }
-    }
-
-    renderGrid(){
-        return this.state.board.map(
-            (box,index) => <div className="box" key={index} onClick={() => this.handleSquareClick(index)}>{box}</div>
-        )
-    }
-
-    render (){
-        return(
-
-            <div className="board">
-                {this.renderGrid()}
-            </div>
-
-        );
-    }
-
+  render() {
+    return <div className="board">{this.renderGrid()}</div>;
+  }
 }
 
 export default Game;
--- src/Tests/Game.test.js
+++ src/Tests/Game.test.js
@@ -1,166 +1,181 @@
-import React from 'react';
-import { shallow , mount } from 'enzyme';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Game from '../Components/Game/Game';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Game from "../Components/Game/Game";
 
 configure({ adapter: new Adapter() });
 
-describe('<Game />', () => {
-	it('Test if 3x3 grid is formed in game component', () => {
-		let wrapper = shallow(<Game />);
-		expect(wrapper.find('div.box')).toHaveLength(9);
+describe("<Game />", () => {
+  it("Test if 3x3 grid is formed in game component", () => {
+    let wrapper = shallow(<Game />);
+    expect(wrapper.find("div.box")).toHaveLength(9);
+  });
+
+  it("Test if handleSquareClick is triggered on click of square", () => {
+    let wrapper = shallow(<Game />);
+    let spy = jest.spyOn(wrapper.instance(), "handleSquareClick");
+    wrapper
+      .find("div.box")
+      .first()
+      .simulate("click");
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("Test if handleSquareClick is updating board", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: Array(9).fill(null),
+      currentPlayer: "X"
     });
+    wrapper.instance().handleSquareClick(5);
+    expect(wrapper.instance().state.board[5]).toEqual("X");
+  });
 
-    it('Test if handleSquareClick is triggered on click of square', () => {
-        let wrapper = shallow(<Game />);
-        let spy = jest.spyOn(wrapper.instance(), 'handleSquareClick');
-        wrapper.find('div.box').first().simulate('click');
-        expect(spy).toHaveBeenCalled();
+  it("Test if handleSquareClick is updating currentPlayer to O after X", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: Array(9).fill(null),
+      currentPlayer: "X"
     });
+    wrapper.instance().handleSquareClick(5);
+    expect(wrapper.instance().state.currentPlayer).toEqual("O");
+  });
 
-    it('Test if handleSquareClick is updating board', () => {
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : Array(9).fill(null),
-            currentPlayer : "X"
-        })
-        wrapper.instance().handleSquareClick(5);
-        expect(wrapper.instance().state.board[5]).toEqual("X");
+  it("Test if handleSquareClick is updating alternatly X and O on click", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: Array(9).fill(null),
+      currentPlayer: "X"
     });
+    wrapper.instance().handleSquareClick(5);
+    expect(wrapper.instance().state.board[5]).toEqual("X");
+    wrapper.instance().handleSquareClick(0);
+    expect(wrapper.instance().state.board[0]).toEqual("O");
+  });
 
-    it('Test if handleSquareClick is updating currentPlayer to O after X', () => {
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : Array(9).fill(null),
-            currentPlayer : "X"
-        })
-        wrapper.instance().handleSquareClick(5);
-        expect(wrapper.instance().state.currentPlayer).toEqual("O");
+  it("Test if handleSquareClick is not updating currentPlayer on multiclicks", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: Array(9).fill(null),
+      currentPlayer: "X"
     });
+    wrapper.instance().handleSquareClick(5);
+    expect(wrapper.instance().state.board[5]).toEqual("X");
+    expect(wrapper.instance().state.currentPlayer).toEqual("O");
+    wrapper.instance().handleSquareClick(5);
+    expect(wrapper.instance().state.currentPlayer).toEqual("O");
+  });
 
-    it('Test if handleSquareClick is updating alternatly X and O on click', () => {
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : Array(9).fill(null),
-            currentPlayer : "X"
-        })
-        wrapper.instance().handleSquareClick(5);
-        expect(wrapper.instance().state.board[5]).toEqual("X");
-        wrapper.instance().handleSquareClick(0);
-        expect(wrapper.instance().state.board[0]).toEqual("O");
+  it("Test if handleSquareClick is not updating board on multiclicks", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: Array(9).fill(null),
+      currentPlayer: "X"
     });
+    wrapper.instance().handleSquareClick(5);
+    expect(wrapper.instance().state.board[5]).toEqual("X");
+    wrapper.instance().handleSquareClick(5);
+    expect(wrapper.instance().state.board[5]).toEqual("X");
+  });
 
-    it('Test if handleSquareClick is not updating currentPlayer on multiclicks', () => {
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board: Array(9).fill(null),
-            currentPlayer : "X"
-        })
-        wrapper.instance().handleSquareClick(5);
-        expect(wrapper.instance().state.board[5]).toEqual("X");
-        expect(wrapper.instance().state.currentPlayer).toEqual("O");
-        wrapper.instance().handleSquareClick(5);
-        expect(wrapper.instance().state.currentPlayer).toEqual("O");
-    })
-
-    it('Test if handleSquareClick is not updating board on multiclicks', () => {
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : Array(9).fill(null),
-            currentPlayer: "X"
-        })
-        wrapper.instance().handleSquareClick(5);
-        expect(wrapper.instance().state.board[5]).toEqual("X");
-        wrapper.instance().handleSquareClick(5);
-        expect(wrapper.instance().state.board[5]).toEqual("X");
-    })
-
-    it('Test if checkWinner is setting winner horizontally' , () =>{
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : ["X","X","X","O","O",null,null,null,null],
-            currentPlayer : "X",
-            winner: null
-        })
-        wrapper.instance().checkWinner();
-        expect(wrapper.instance().state.winner).toEqual("X");
-    })
-
-    it('Test if checkWinner is setting winner horizontally' , () =>{
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : ["O","O", null,"X","X","X",null,null,null],
-            currentPlayer : "X",
-            winner: null
-        })
-        wrapper.instance().checkWinner();
-        expect(wrapper.instance().state.winner).toEqual("X");
-    })
-
-    it('Test if checkWinner is setting winner horizontally' , () =>{
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : ["X","X", null,"X",null,null,"O","O","O"],
-            currentPlayer : "O",
-            winner: null
-        })
-        wrapper.instance().checkWinner();
-        expect(wrapper.instance().state.winner).toEqual("O");
-    })
-
-    it('Test if checkWinner is setting winner vertically' , () => {
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : ["X","O",null,"X",null,null,"X",null,"O"],
-            currentPlayer:"X",
-            winner : null
-        })
-        wrapper.instance().checkWinner();
-        expect(wrapper.instance().state.winner).toEqual("X");
-    })
-
-    it('Test if checkWinner is setting winner vertically', () => {
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board :["X","O",null,"X","O",null,"X","O","O"],
-            currentPlayer : "O",
-            winner: null
-        })
-        wrapper.instance().checkWinner();
-        expect(wrapper.instance().state.winner).toEqual("O");
+  it("Test if checkWinner is setting winner horizontally", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: ["X", "X", "X", "O", "O", null, null, null, null],
+      currentPlayer: "X",
+      winner: null
     });
+    wrapper.instance().checkWinner();
+    expect(wrapper.instance().state.winner).toEqual("X");
+  });
 
-    it('Test if checkWinner is setting winner vertically', () => {
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board :["O","X","X","X",null,"X","O","O","X"],
-            currentPlayer : "X",
-            winner: null
-        })
-        wrapper.instance().checkWinner();
-        expect(wrapper.instance().state.winner).toEqual("X");
+  it("Test if checkWinner is setting winner horizontally", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: ["O", "O", null, "X", "X", "X", null, null, null],
+      currentPlayer: "X",
+      winner: null
     });
+    wrapper.instance().checkWinner();
+    expect(wrapper.instance().state.winner).toEqual("X");
+  });
 
-    it('Test if checkWinner is setting winner diagonally', ()=>{
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : ["O","X","X",null,"O","X","O","X","O"],
-            currentPlayer: "O",
-            winner : null
-        })
-        wrapper.instance().checkWinner();
-        expect(wrapper.instance().state.winner).toEqual("O");
-    })
+  it("Test if checkWinner is setting winner horizontally", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: ["X", "X", null, "X", null, null, "O", "O", "O"],
+      currentPlayer: "O",
+      winner: null
+    });
+    wrapper.instance().checkWinner();
+    expect(wrapper.instance().state.winner).toEqual("O");
+  });
 
-    it('Test if checkWinner is setting winner diagonally', ()=>{
-        let wrapper = shallow(<Game />);
-        wrapper.setState({
-            board : ["O","X","X","X","X",null,"X","O",null],
-            currentPlayer: "X",
-            winner : null
-        })
-        wrapper.instance().checkWinner();
-        expect(wrapper.instance().state.winner).toEqual("X");
-    })
-})
+  it("Test if checkWinner is setting winner vertically", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: ["X", "O", null, "X", null, null, "X", null, "O"],
+      currentPlayer: "X",
+      winner: null
+    });
+    wrapper.instance().checkWinner();
+    expect(wrapper.instance().state.winner).toEqual("X");
+  });
+
+  it("Test if checkWinner is setting winner vertically", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: ["X", "O", null, "X", "O", null, "X", "O", "O"],
+      currentPlayer: "O",
+      winner: null
+    });
+    wrapper.instance().checkWinner();
+    expect(wrapper.instance().state.winner).toEqual("O");
+  });
+
+  it("Test if checkWinner is setting winner vertically", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: ["O", "X", "X", "X", null, "X", "O", "O", "X"],
+      currentPlayer: "X",
+      winner: null
+    });
+    wrapper.instance().checkWinner();
+    expect(wrapper.instance().state.winner).toEqual("X");
+  });
+
+  it("Test if checkWinner is setting winner diagonally", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: ["O", "X", "X", null, "O", "X", "O", "X", "O"],
+      currentPlayer: "O",
+      winner: null
+    });
+    wrapper.instance().checkWinner();
+    expect(wrapper.instance().state.winner).toEqual("O");
+  });
+
+  it("Test if checkWinner is setting winner diagonally", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+      board: ["O", "X", "X", "X", "X", null, "X", "O", null],
+      currentPlayer: "X",
+      winner: null
+    });
+    wrapper.instance().checkWinner();
+    expect(wrapper.instance().state.winner).toEqual("X");
+  });
+
+  it("Test if handleSquareClick is not updating board and currentPlayer after a win", () => {
+    let wrapper = shallow(<Game />);
+    wrapper.setState({
+        board: ["X", "X", null, "X", null, null, "O", "O", "O"],
+        currentPlayer: "X",
+        winner: "O"
+    });
+    wrapper.instance().handleSquareClick(2);
+    expect(wrapper.instance().state.board[2]).toEqual(null);
+    expect(wrapper.instance().state.currentPlayer).toEqual("X");
+  });
+});
